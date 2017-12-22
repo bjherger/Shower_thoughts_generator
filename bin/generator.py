45edc2654fa11f80bafa48f83b0cb41b740a1298
Brendan J. Herger <b@revup.com>
2017-12-22 11:27:02 -0800

    Interface for predict

---
+++ bin/generator.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""
+coding=utf-8
+
+Code Template
+
+"""
+import logging
+import os
+
+import cPickle
+import numpy
+import numpy as np
+import re
+
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras.optimizers import RMSprop
+
+import lib
+import models
+from sentence_callback import SentenceGenerator
+from reddit_scraper import scrape_subreddit
+
+
+def main():
+    """
+    Main function documentation template
+    :return: None
+    :rtype: None
+    """
+    logging.basicConfig(level=logging.DEBUG)
+
+    char_model, observations = extract()
+    char_model, observations = transform(char_model, observations)
+    load(char_model, observations)
+
+    pass
+
+def extract():
+    # TODO Extract
+
+    # TODO Extract appropriate model
+    char_model = None
+
+    # TODO Extract posts to be completed
+    observations = None
+
+    logging.info('End extract')
+    lib.archive_dataset_schemas('extract', locals(), globals())
+    return char_model, observations
+
+
+def transform(char_model, observations):
+
+    # TODO Normalize post seed text
+
+    # TODO Transform post_seeds into X, y
+    x = None
+    y = None
+
+    # TODO Infer sentence, add to to observations
+
+    return char_model, observations
+
+
+def load(char_model, observations):
+    # TODO Export observations
+
+    pass
+
+# Main section
+if __name__ == '__main__':
+    main()
