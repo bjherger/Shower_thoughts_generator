213ab22d3819f7ceeb50a866f2c3aa3bef536b2f
Brendan J. Herger <b@revup.com>
2017-12-26 13:34:24 -0800

    First pass at viz interface

---
+++ bin/post_viz.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+coding=utf-8
+
+Code Template
+
+"""
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    """
+    Main function documentation template
+    :return: None
+    :rtype: None
+    """
+    # TODO Reference Variables
+
+
+    # TODO Iterate through posts
+
+    # TODO Feed each seed and respose to helper method
+    create_viz()
+
+
+
+    pass
+
+
+def create_viz():
+
+    # TODO Reference variables
+
+    # TODO Set up chunks (new word, each individual character)
+
+    # TODO Iterage through each chunk
+
+    # TODO Create image of start to curent chunk
+
+    # TODO Add to running GIF
+
+    # TODO Save final GIF
+    pass
+
+# Main section
+if __name__ == '__main__':
+    main()
